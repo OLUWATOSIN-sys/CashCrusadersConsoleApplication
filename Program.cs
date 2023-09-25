39b589d83eada82d6e48a99f4ce60c9731335556
Oluwatosin Olaniran <oluwatosinolaniran1997@gmail.com>
2023-09-25 10:28:11 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CharacterCountAndStringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input string: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Input character: ");
+            char targetChar = Console.ReadLine()[0]; 
+
+            int charCount = CountCharacterOccurrences(input, targetChar);
+            string reversedString = ReverseString(input);
+
+            Console.WriteLine($"Occurrences of '{targetChar}' in the input string: {charCount}");
+            Console.WriteLine("Reversed input string: " + reversedString);
+        }
+
+        static int CountCharacterOccurrences(string input, char targetChar)
+        {
+            int count = 0;
+            foreach (char c in input)
+            {
+                if (c == targetChar)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
